e417006d92b18b5f95e82f361d5e3f00047bed7d
JaiThakur1951 <56255439+JaiThakur1951@users.noreply.github.com>
2026-07-27 20:26:41 +0530

    Update account-requests.tf

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -2,28 +2,28 @@ module "account_request_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "noc+sandboxacc3@writerinformation.com"
-    AccountName               = "Sandbox-Acc3"
-    ManagedOrganizationalUnit = "Sandbox" 
-    SSOUserEmail              = "noc+sandboxacc3@writerinformation.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "Acc3"
+    AccountEmail              = "noc+infraacc2@writerinformation.com"
+    AccountName               = "Infrastructure-Acc2"
+    ManagedOrganizationalUnit = "Infrastructure-OU" 
+    SSOUserEmail              = "noc+infraacc2@writerinformation.com"
+    SSOUserFirstName          = "Infrastructure"
+    SSOUserLastName           = "Acc2"
   }
 
   account_tags = {
-    "ABC:Owner"       = "noc+sandboxacc3@writerinformation.com"
+    "ABC:Owner"       = "noc+infraacc2@writerinformation.com"
     "ABC:Division"    = "CDS"
     "ABC:Environment" = "Testing"
-    "ABC:CostCenter"  = "400059"
+    "ABC:CostCenter"  = "400057"
     "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "104"
-    "ABC:BUCode"      = "AFT004"
-    "ABC:Project"     = "123456"
+    "ABC:DivCode"     = "105"
+    "ABC:BUCode"      = "AFT005"
+    "ABC:Project"     = "456789"
   }
 
   change_management_parameters = {
-    change_requested_by = "AWS Control Tower Lab Sandbox Account"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT) - Sandbox"
+    change_requested_by = "AWS Control Tower Lab Infrastructure Account 2"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT) - Infrastructure 2"
   }
 
   custom_fields = {
@@ -31,5 +31,5 @@ module "account_request_01" {
     custom2 = "b"
   }
 
-  account_customizations_name = "Sandbox"
+  account_customizations_name = "Infrastructure"
 }
