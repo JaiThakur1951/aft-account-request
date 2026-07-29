7ec935412272b4b694503bd8b4bb35524761dea7
JaiThakur1951 <56255439+JaiThakur1951@users.noreply.github.com>
2026-07-29 15:01:24 +0530

    Update account-requests.tf

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -2,28 +2,28 @@ module "account_request_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "noc+infraacc8@writerinformation.com"
-    AccountName               = "Infrastructure-Acc8"
+    AccountEmail              = "noc+infraacc9@writerinformation.com"
+    AccountName               = "Infrastructure-Acc9"
     ManagedOrganizationalUnit = "Infrastructure-OU" 
-    SSOUserEmail              = "noc+infraacc8@writerinformation.com"
+    SSOUserEmail              = "noc+infraacc9@writerinformation.com"
     SSOUserFirstName          = "Infrastructure"
-    SSOUserLastName           = "Acc8"
+    SSOUserLastName           = "Acc9"
   }
 
   account_tags = {
-    "ABC:Owner"       = "noc+infraacc8@writerinformation.com"
+    "ABC:Owner"       = "noc+infraacc9@writerinformation.com"
     "ABC:Division"    = "CDS"
     "ABC:Environment" = "Testing"
-    "ABC:CostCenter"  = "400061"
+    "ABC:CostCenter"  = "400062"
     "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "110"
-    "ABC:BUCode"      = "AFT008"
+    "ABC:DivCode"     = "111"
+    "ABC:BUCode"      = "AFT009"
     "ABC:Project"     = "459789"
   }
 
   change_management_parameters = {
-    change_requested_by = "AWS Control Tower Lab Infrastructure Account 8"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT) - Infrastructure 8"
+    change_requested_by = "AWS Control Tower Lab Infrastructure Account 9"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT) - Infrastructure 9"
   }
 
   custom_fields = {
